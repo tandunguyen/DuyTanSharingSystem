07f45f9c51fac9d9231340a6cc49d1950ab8e595
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-14 11:00:00 +0700

    Domain: Dinh nghia IBaseRepository

---
+++ Domain/Interface/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+namespace Domain.Interface
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        IQueryable<T> GetAll();
+        Task<T?> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<bool> DeleteAsync(Guid id);
+        Task BulkUpdateAsync(List<T> entities);
+        //AddRangeAsync
+        Task AddRangeAsync(List<T> entities);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    }
+}
