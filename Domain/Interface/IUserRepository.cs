05c09716d8ad1f35dccc5b75faa39176e5b01415
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-14 13:00:00 +0700

    Domain: Interface quan ly nguoi dung

---
+++ Domain/Interface/IUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+     public interface IUserRepository : IBaseRepository<User>
+    {
+        
+        Task<bool> GetExsitsEmailAsync(string email);
+        Task<User?> GetUserByEmailAsync(string email);
+        //tim kiem nguoi (dang)
+        Task<User?> GetUserByIdAsync(Guid userId);
+        Task<List<User>> SearchUsersAsync(string keyword);
+        Task<string?> GetFullNameByIdAsync(Guid id);
+        Task<List<User>> GetAllUsersAsync();
+        Task<List<User>> GetUsersByIdsAsync(List<Guid> userIds);
+        Task<IEnumerable<User>> GetAdminsAsync();
+        Task<User?> GetAdminByIdAsync(Guid adminId);
+        Task<bool> ExistUsersAsync(Guid userId);
+        Task<IEnumerable<(DateTime Date, int Count)>> GetUserTrendAsync(string timeRange);
+        Task<IEnumerable<(string TrustCategory, int Count)>> GetUserTrustDistributionAsync();
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetUserBySuggestFriendAsync(Guid userId);
+        Task<List<User>> GetAllActiveUsersAsync();
+        Task<List<User>> GetUsersWithDetailsAsync();
+    }
+}
