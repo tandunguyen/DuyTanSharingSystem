584e0548762a5eeb684cf9b28766cdbe72b8da91
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-14 15:00:00 +0700

    Domain: Interface quan ly bai dang

---
+++ Domain/Interface/IPostRepository.cs
@@ -0,0 +1,37 @@
+﻿using static Domain.Common.Enums;
+
+namespace Domain.Interface
+{
+    public interface IPostRepository : IBaseRepository<Post>
+    {
+        Task<IEnumerable<Post>> GetPostsByApprovalStatusAsync(ApprovalStatusEnum approvalStatusEnum);
+        Task<List<Post>> GetPostsByTypeAsync(PostTypeEnum postType, Guid? lastPostId, int pageSize, CancellationToken cancellationToken);
+
+        Task<List<Post>> SearchPostsAsync(string keyword);
+
+        Task<List<Post>> GetAllPostsAsync(Guid? lastPostId, int pageSize, CancellationToken cancellationToken);
+
+        //xoa mem
+      
+        Task SoftDeletePostAsync(Guid postId);
+        // ✅ Thêm phương thức tìm kiếm bài viết(dangg)
+        Task<List<Post>> SearchPostsAsync(string keyword, DateTime? fromDate, DateTime? toDate,int? Year, int? Month,int? Day);
+
+
+        Task<List<Post>> GetSharedPostAllAsync(Guid originalPostId);
+
+        Task<List<Post>> GetPostsByOwnerAsync(Guid userId, Guid? lastPostId, int pageSize, CancellationToken cancellationToken);
+        Task<Post?> GetByIdOriginalPostAsync(Guid id);
+        Task<Guid> GetPostOwnerIdAsync(Guid postId);
+
+        Task<List<Post>> GetAllPostForSearchAI();
+        Task<int> GetPostCountAsync(Guid userId);
+        Task<List<Post>> GetAllPostsWithReportsAsync();
+        Task<List<Post>> GetPostImagesByUserAsync(Guid userId);
+        Task<List<Post>> GetTopPostImagesByUserAsync(Guid userId, int count = 6);
+
+        Task<List<Post>> GetAllPostsForAdminAsync(int skip, int take, CancellationToken cancellationToken);
+        Task<int> GetTotalPostCountAsync(CancellationToken cancellationToken);
+
+    }
+}
