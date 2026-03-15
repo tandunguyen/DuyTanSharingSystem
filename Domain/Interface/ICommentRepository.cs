25367d6d5960f8cf37ab743cde23a47e6d9cba5f
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-15 14:00:00 +0700

    Domain: Interface binh luan

---
+++ Domain/Interface/ICommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface ICommentRepository : IBaseRepository<Comment>
+    {
+/*        Task<IEnumerable<Comment>> GetCommentByPostIdAsync(Guid postId);*/
+        Task<int> CountPostCommentAsync(Expression<Func<Comment, bool>> predicate);
+        Task<Comment?> GetCommentByIdAsync(Guid commentId);
+        //lấy các comment mà user đã bình luận
+        Task<List<Comment>> GetAllCommentByUserIdAsync(Guid userId);
+        Task<List<Comment>> GetReplysCommentAllAsync(Guid parentCommentId);
+        Task<(List<Comment>, int)> GetCommentByPostIdAsync(Guid postId, int page, int pageSize);
+        Task<List<Comment>> GetCommentsByPostIdDeleteAsync(Guid postId);
+        Task<List<Comment>> GetRepliesByCommentIdAsync(Guid parentCommentId);
+
+        Task<int> GetCommentCountAsync(Guid userId);
+
+        Task<int> CountRepliesAsync(Guid parentCommentId);
+        bool HasMoreReplies(Guid commentId);
+        Task<Guid> GetCommentOwnerIdAsync(Guid commentId);
+        Task<List<Comment>> GetRepliesByCommentIdWithCursorAsync(Guid parentCommentId, Guid? lastReplyId, int pageSize, CancellationToken cancellationToken);
+        Task<List<Comment>> GetCommentsByPostIdWithCursorAsync(Guid postId, Guid? lastCommentId, int pageSize, CancellationToken cancellationToken);
+        Task<IEnumerable<(DateTime Date, int Count)>> GetCommentsOverTimeAsync(string timeRange);
+    }
+}
