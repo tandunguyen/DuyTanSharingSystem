766bd0d8c9966b909c621843e805984eea54c8c5
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-15 16:00:00 +0700

    Domain: Interface lich su AI

---
+++ Domain/Interface/IAIChatHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IAIChatHistoryRepository : IBaseRepository<AIChatHistory>
+    {
+        Task<List<AIChatHistory>> GetHistoriesByConversationId(Guid conversationId, Guid? lastMessageId, int pageSize);
+        Task<List<AIChatHistory>> GetHistoriesByConversationId(Guid conversationId);
+        Task DeleteRangeAsync(IEnumerable<Guid> ids);
+    }
+}
