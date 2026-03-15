096c8047f6e14ac83f56316ebda7a0e7a2220c97
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-15 09:00:00 +0700

    Domain: Interface quan ly chung xe

---
+++ Domain/Interface/IRideRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Domain.Common.Enums;
+
+namespace Domain.Interface
+{
+    public interface IRideRepository : IBaseRepository<Ride>
+    {
+        Task<IEnumerable<Ride>> GetActiveRidesAsync();
+        Task<Ride?> GetRideByUserIdAsync(Guid userId);
+        Task<IEnumerable<Ride>> GetActiveRidesByPassengerIdAsync(Guid passengerId);
+        Task<IEnumerable<Ride>> GetActiveRidesByDriverIdIdAsync(Guid driverId);
+        Task<int> GetDriveRideCountAsync(Guid userId);
+        Task<int> GetPassengerRideCountAsync(Guid userId);
+        Task<List<Ride>> GetRidePostsByPassengerIdAsync(Guid passengerId, Guid? lastPostId, int pageSize);
+        Task<List<Ride>> GetRidePostsByDriverIdAsync(Guid driverId, Guid? lastPostId, int pageSize);
+        Task<List<Ride>> GetActiveRidesByDriverIdAsync(Guid driverId);
+        Task<List<Ride>> GetCompletedRidesWithRatingAsync(Guid driverId);
+        Task<(List<Ride> Rides, int TotalRecords)> GetRidesByStatusAsync(StatusRideEnum status, int page, int pageSize);
+        Task<Ride?> GetRideDetailsAsync(Guid id);
+        Task<List<Ride>> GetRidesByTimeRangeAsync(DateTime? startDate, DateTime? endDate);
+    }
+}
