d35f03ea09a5f49a6d00c1f69a72d5834d6112f0
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-16 10:00:00 +0700

    Domain: Trien khai UnitOfWork Interface

---
+++ Domain/Interface/IUnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        IEmailTokenRepository EmailTokenRepository { get; }
+        IPostRepository PostRepository { get; }
+        ILikeRepository LikeRepository { get; }
+        IRefreshtokenRepository RefreshtokenRepository { get; }
+        IUserReportRepository UserReportRepository { get; }
+
+        IShareRepository ShareRepository { get; }
+        ICommentRepository CommentRepository { get; }
+        ICommentLikeRepository CommentLikeRepository { get; }
+
+        IRidePostRepository RidePostRepository { get; }
+        IRideRepository RideRepository { get; }
+        ILocationUpdateRepository LocationUpdateRepository { get; }
+
+        IReportRepository ReportRepository { get; }
+        IRideReportRepository RideReportRepository { get; }
+        IRatingRepository RatingRepository { get; }
+
+        IConversationRepository ConversationRepository { get; }
+        IMessageRepository MessageRepository { get; }
+      
+        IFriendshipRepository FriendshipRepository { get; }
+
+        INotificationRepository NotificationRepository { get; }
+
+
+        IAIConversationRepository AIConversationRepository { get; }
+        IAIChatHistoryRepository AIChatHistoryRepository { get; }
+        IUserScoreHistoriesRepository UserScoreHistoriesRepository { get; }
+
+        IAccommodationPostRepository AccommodationPostRepository { get; }
+        IAccommodationReviewRepository AccommodationReviewRepository { get; }
+
+        IStudyMaterialRepository StudyMaterialRepository { get; }
+        IStudyMaterialRatingRepository StudyMaterialRatingRepository { get; }
+
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync(); // ✅ Bắt đầu giao dịch
+        Task CommitTransactionAsync(); // ✅ Hoàn tất giao dịch
+        Task RollbackTransactionAsync();
+    }
+}
