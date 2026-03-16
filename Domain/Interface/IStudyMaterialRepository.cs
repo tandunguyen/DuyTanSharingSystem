b5956bf572211f20f340e5890136f3ecdb649694
tandunguyen <nguyentandung02012004@gmail.com>
2026-03-16 08:30:00 +0700

    Domain: Interface tai lieu hoc tap

---
+++ Domain/Interface/IStudyMaterialRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IStudyMaterialRepository : IBaseRepository<StudyMaterial>
+    {
+        Task <List<StudyMaterial>> GetAllStudyMaterialAsync(Guid? lastLastStudyMaterialIdId, int pageSize);
+        Task<long> GetTotalFileSizeByUserAsync(Guid userId);
+    }
+}
