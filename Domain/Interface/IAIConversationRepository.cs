0bd635e6e0619036f6ba7a7b3227dd0bf381e965
hieuduongdinh2 <hieuduongdinh2@gmail.com>
2026-03-17 08:30:00 +0700

    Infra: Interface quan ly hoi thoai AI

---
+++ Domain/Interface/IAIConversationRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Domain.Interface
+{
+    public interface IAIConversationRepository : IBaseRepository<AIConversation>
+    {
+        Task<AIConversation?> GetConversationByUserId(Guid id);
+        Task<List<AIConversation>> GetConversationsByUserId(Guid userId, Guid? lastConversationId, int pageSize);
+        Task<AIConversation?> GetByIdAsync(Guid? id);
+    }
+}
