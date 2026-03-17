62cba5631b60c965165871e9804ff596b543bc4e
hieuduongdinh2 <hieuduongdinh2@gmail.com>
2026-03-17 14:00:00 +0700

    Infra: Interface quan ly cuoc hoi thoai

---
+++ Domain/Interface/IConversationRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IConversationRepository :IBaseRepository<Conversation>
+    {
+        Task<Conversation?> GetConversationAsync(Guid userId1, Guid userId2);
+        Task<List<Conversation>> GetAllConversationsAsync(Guid userId);
+        Task<List<Conversation>> GetManyAsync(Expression<Func<Conversation, bool>> predicate);
+    }   
+}
