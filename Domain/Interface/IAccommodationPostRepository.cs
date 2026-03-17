6676282cd83be0ae76da3514461ea9cd42105951
hieuduongdinh2 <hieuduongdinh2@gmail.com>
2026-03-17 10:00:00 +0700

    Infra: Interface quan ly thong tin tim tro

---
+++ Domain/Interface/IAccommodationPostRepository.cs
@@ -0,0 +1,25 @@
+﻿// File: Domain/Interface/IAccommodationPostRepository.cs (Sửa đổi)
+
+using Domain.Entities;
+using System.Linq.Expressions;
+using NetTopologySuite.Geometries;
+
+namespace Domain.Interface
+{
+    public interface IAccommodationPostRepository : IBaseRepository<AccommodationPost>
+    {
+        // Hàm mới: Lấy danh sách mặc định (không lọc theo vị trí)
+        Task<List<AccommodationPost>> GetAllAccommodationPostAsync(Guid? lastPostId, int pageSize);
+
+        // Hàm cũ: Tìm kiếm chính (dùng khi có tọa độ, bộ lọc vị trí)
+        Task<List<AccommodationPost>> GetAccommodationPostsForSearchAsync(
+            Point targetLocation,
+            double radiusMeters,
+            decimal? priceMin,
+            decimal? priceMax,
+            string? roomType,
+            Guid? lastPostId,
+            int pageSize);
+        //Get by id
+    }
+}
