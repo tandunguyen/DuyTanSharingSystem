d3f4fa3120dbac2e3554159e5968b2f6decba439
hieuduongdinh2 <hieuduongdinh2@gmail.com>
2026-03-18 16:00:00 +0700

    Infra: Interface quan ly bai dang chung xe

---
+++ Domain/Interface/IRidePostRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IRidePostRepository : IBaseRepository<RidePost>
+    {
+        Task<RidePost?> GetByDriverIdAsync(Guid userId);
+        Task<List<RidePost>> GetAllRidePostAsync(Guid? lastPostId, int pageSize);
+        Task<List<RidePost>> GetAllRidePostForOwnerAsync(Guid? lastPostId, int pageSize,Guid ownerId);
+        Task<List<RidePost>> GetAllRidePostForSearchAI();
+        Task<int> GetRidePostCountAsync(Guid userId);
+        Task<(string start, string end, string startL, string EndL)> GetLatLonByRidePostIdAsync(Guid id);
+        Task<RidePost?> FindAsync(Guid id);
+
+    }
+}
