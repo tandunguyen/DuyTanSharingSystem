aa1f31a5a8c99bbdc2364c9e9e0aab37e05f148d
hieuduongdinh2 <hieuduongdinh2@gmail.com>
2026-03-18 14:00:00 +0700

    Infra: Interface bao cao nguoi dung vi pham

---
+++ Domain/Interface/IUserReportRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Interface
+{
+    public interface IUserReportRepository : IBaseRepository<UserReport>
+    {       
+        Task<IEnumerable<UserReport>> GetReportsByUserIdAsync(Guid reportedUserId);
+        Task<IEnumerable<UserReport>> GetAllUserReportAsync();
+    }
+}
