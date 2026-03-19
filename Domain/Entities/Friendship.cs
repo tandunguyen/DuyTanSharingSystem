543238aa6e8644902b1d51b712e03dae8371edf3
Voduc822003 <Voduc822003@gmail.com>
2026-03-19 14:00:00 +0700

    App: Thuc the quan ly moi quan he ban be

---
+++ Domain/Entities/Friendship.cs
@@ -0,0 +1,67 @@
+﻿
+using static Domain.Common.Enums;
+
+namespace Domain.Entities
+{
+    public class Friendship
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; } // Không nên nullable, một quan hệ bạn bè cần có 2 người
+        public Guid FriendId { get; private set; } // Không nên nullable
+        public DateTime CreatedAt { get; private set; }
+        public FriendshipStatusEnum Status { get; private set; } // Trạng thái kết bạn
+        public DateTime? UpdatedAt { get; private set; }
+        public Friendship(Guid userId, Guid friendId)
+        {
+            if (userId == Guid.Empty) throw new ArgumentException("UserId cannot be empty.");
+            if (friendId == Guid.Empty) throw new ArgumentException("FriendId cannot be empty.");
+            if (userId == friendId) throw new ArgumentException("UserId and FriendId cannot be the same.");
+
+            Id = Guid.NewGuid();
+            UserId = userId;
+            FriendId = friendId;
+            CreatedAt = DateTime.UtcNow;
+            Status = FriendshipStatusEnum.Pending; // Mặc định là yêu cầu đang chờ
+        }
+
+        /// <summary>
+        /// Xác nhận yêu cầu kết bạn
+        /// </summary>
+        public void Accept()
+        {
+            Status = FriendshipStatusEnum.Accepted;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Hủy hoặc từ chối yêu cầu kết bạn
+        /// </summary>
+        public void Reject()
+        {
+            Status = FriendshipStatusEnum.Rejected;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Hủy kết bạn
+        /// </summary>
+        public void Remove()
+        {
+            Status = FriendshipStatusEnum.Removed;
+            UpdatedAt = DateTime.UtcNow;
+        }
+        public void Reactivate()
+        {
+            if (Status == FriendshipStatusEnum.Removed)
+            {
+                Status = FriendshipStatusEnum.Pending;
+                UpdatedAt = DateTime.UtcNow;
+            }
+            else
+            {
+                throw new InvalidOperationException("Không thể kích hoạt lại lời mời kết bạn không ở trạng thái Removed.");
+            }
+        }
+    }
+}
+
